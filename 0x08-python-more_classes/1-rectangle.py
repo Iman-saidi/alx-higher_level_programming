b3cf76a9aee2d38dc404b3639363fce6dc6832ef
Iman-saidi <summeiyaiman@gmail.com>
2024-07-09 13:39:20 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,12 +26,12 @@ class Rectangle:
         self.__width = value
 
     @property
-        def height(self):
-            """getter for the private instance attribute height"""
-            return self.__height
+    def height(self):
+        """getter for the private instance attribute height"""
+        return self.__height
 
     @height.setter
-        def height(self, value)
+    def height(self, value)
         """setter for the private instance attribute height"""
         if type (value) is not int
             raise TypeError("height must be an integer")
